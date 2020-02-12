6642ae43bff28a53dee799ac10da664dd4181a2f
anujaagaitonde <anujaagaitonde@gmail.com>
2020-02-12 21:03:28 +0000

    camera app test

---
+++ camera-app/app.js
@@ -0,0 +1,32 @@
+var constraints = { video: {facingMode: "user" }, audio = false};
+
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+
+// Access camera and stream to camera--view
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+
+// Program camera trigger button to grab a frame from the stream to use as image output
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
+
+// Start camera once window finished loading
+window.addEventListener("load", cameraStart, false);
+
