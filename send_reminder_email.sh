76c9898f0a437ce70c42f1818d8f6c82e097f4af
anujaagaitonde <anujaagaitonde@gmail.com>
2020-05-28 20:13:23 +0100

    add executable shell script

--- send_reminder_email.sh
+++ send_reminder_email.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 /Users/anujagaitonde/Documents/Uni/EE4\ FYP/Attendance\ Monitoring\ App/venv/bin/python /Users/anujagaitonde/Documents/Uni/EE4\ FYP/Attendance\ Monitoring\ App/attendance_app_django/manage.py send_reminder_email
