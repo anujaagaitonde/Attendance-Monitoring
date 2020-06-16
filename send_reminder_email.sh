225aa38425ee90a5c90bc177f2b0c09939136902
anujaagaitonde <anujaagaitonde@gmail.com>
2020-06-16 17:04:02 +0100

    cleanup shell

--- send_reminder_email.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/Users/anujagaitonde/Documents/Uni/EE4\ FYP/Attendance\ Monitoring\ App/venv/bin/python /Users/anujagaitonde/Documents/Uni/EE4\ FYP/Attendance\ Monitoring\ App/attendance_app_django/manage.py send_reminder_email
